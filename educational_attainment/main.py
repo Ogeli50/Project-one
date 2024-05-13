7edd32429b6a19b067020c903795b024dbad2d55
Ogeli50 <ob.tasie50@gmail.com>
2024-05-13 07:29:30 -0500

    mofified data

--- educational_attainment/main.py
+++ educational_attainment/main.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+Education2018 = pd. read_csv("../data/Education_2018.csv")
+Education2022 = pd. read_csv("..data/Eduacation_2022.csv")
+
+print (Education2018.tail(20))
+print (Education2022.tail(20))
